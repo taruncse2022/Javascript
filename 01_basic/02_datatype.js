7840599e4e0a1a379378212defa17c9762b65d79
taruncse2022 <118814372+taruncse2022@users.noreply.github.com>
2024-04-27 14:55:49 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(2+2) // give error as we are using nodejs, not browser
+
+console.log(3 
+    +  3) // code readability should be high
+
+console.log("Tarun")
+
+let name = "tarun"
+let age = 23
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 10
+// bigint => used to very high number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not defined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
