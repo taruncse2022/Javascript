7a93eddde0294c5b487b07f47fca79e257d70789
taruncse2022 <118814372+taruncse2022@users.noreply.github.com>
2024-04-27 13:35:40 +0000

    container config

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 95399
+let accountEmail = "Tarun@google.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+let accountState;
+
+
+//accountId = 12 //not allowed
+
+accountEmail = "TS@google.com"
+accountPassword = "1212121"
+accountCity = "Noida"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
