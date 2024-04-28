514ee263013dc065fd68e75d078eae1cf8c6909d
taruncse2022 <118814372+taruncse2022@users.noreply.github.com>
2024-04-28 10:43:11 +0000

    dataype

---
+++ 01_basic/datatypeSummary.js
@@ -0,0 +1,38 @@
+//PRIMITIVE
+// 7 Types = String , Boolean , Null , Number , Undefined , Symbol , BigInt 
+
+const Score = 100
+const isLoggedIn = true 
+const outsideTemp = null
+let userEmail ;
+const id = Symbol('123')
+const newId = Symbol('123')
+//console.log(id === newId);
+const BigInt = 12368712637812312n
+
+//NON PRIMITIVE
+// Arrays , Objects , Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let Obj = {
+    name: "Tarun",
+    age: "24",
+}
+
+const myFunction = function(){
+    //console.log("Hello World ");
+}
+
+// Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function object 
+//        Object  =>  object
